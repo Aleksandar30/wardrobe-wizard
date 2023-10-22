f7fecebfe4a3e599175d821ca5c0d2f40a3b9b59
Aleksandar30 <sasaivanovic11@gmail.com>
2023-10-22 14:29:53 +0200

    before-django

--- src/app/cart/completed-orders/completed-orders.component.ts
+++ src/app/cart/completed-orders/completed-orders.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { CartService } from '../cart.service';
 import { OrderStatus } from 'src/app/enums';
 import { Order } from '../order.model';
 import { OrderService } from '../order.service';
 import { UserService } from 'src/app/auth/users.service';
 import { User } from 'src/app/auth/user.model';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-completed-orders',
   templateUrl: './completed-orders.component.html',
   styleUrls: ['./completed-orders.component.css']
 })
-export class CompletedOrdersComponent {
+export class CompletedOrdersComponent implements AfterViewInit, OnInit {
 
-  orders: Order[] = [];
+  orders = new MatTableDataSource<Order>();
 
-  displayedColumns: string[] = ['name', 'amount', 'status', 'remove'];
+  displayedColumns: string[] = ['shopItem', 'amount', 'status', 'remove'];
+
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
 
   constructor(private orderService: OrderService, private userService: UserService) { }
@@ -24,10 +30,17 @@ export class CompletedOrdersComponent {
     this.orderService.getOrders().subscribe((orders: Order[]) => {
       const currentUser = this.userService.currentUser;
       if (currentUser) {
-        this.orders = orders.filter((o: Order) => o.userId === currentUser.id && o.status !== OrderStatus.InProgress);
+        this.orders.data = orders.filter((o: Order) => o.userId === currentUser.id && o.status !== OrderStatus.InProgress);
+        //this.orders.data = orders;
 
       }
     });
+  }
+
+
+  ngAfterViewInit(): void {
+    this.orders.sort = this.sort;
+    this.orders.paginator = this.paginator;
   }
 
 
